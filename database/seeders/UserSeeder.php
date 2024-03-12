15459bd3f2d4f7f38642aad9f56338e4d79be066
ahmakbardev <ahmakbar.dev@gmail.com>
2024-03-12 20:19:18 +0700

    first push portfolio

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Create an admin user
+        User::create([
+            'name' => 'User',
+            'email' => 'user@example.com',
+            'password' => Hash::make('user123'), // Hash the password for security
+            // 'address' => 'Jl Jombang 2 Malang'
+        ]);
+    }
+}
